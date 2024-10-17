88de45c2f8aa9ac6aee72ba96557a972696fb14e
Zach Angha <112359501+zachangha@users.noreply.github.com>
2024-10-16 19:32:07 -0700

    hitting ball and ball reset

--- Assets/Scenes/handball/Handball.cs
+++ Assets/Scenes/handball/Handball.cs
@@ -1,24 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
+using Palmmedia.ReportGenerator.Core.Parser.Analysis;
 using UnityEngine;
 
 public class Handball : MonoBehaviour
 {
     Vector3 initialPos;
+    private int bounceCount = 0;
+    private bool returning = false;
+    bool test;
+    bool serving;
+
 
     private void Start()
     {
         initialPos = transform.position;
+        serving = false;
     }
 
+    private void Update()
+    {
+        test = BallBounce.hitting;
+    }
 
-    // Too see if ball goes out of bounds
-    // private void onCollisionEnter(Collision collision)
-    // {
-    //     if(collision.transform.CompareTag("Out"))
-    //     {
-    //         GetComponent<Rigidbody>().velocity = Vector3.zero;
-    //         transform.position = initialPos;
-    //     }
-    // }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.CompareTag("Court"))
+        {
+            bounceCount++;
+            Debug.Log("Bounces: " + bounceCount);
+        }
+
+        if(collision.transform.CompareTag("Ground"))
+        {
+            ResetBall();
+        }
+
+        if(collision.transform.CompareTag("Wall") && bounceCount != 1)
+        {
+            bounceCount = 0;
+        }
+
+        if(bounceCount >= 2 && returning == false)
+        {
+            ResetBall();
+        }
+
+        if(test == true)
+        {
+            bounceCount = 0;
+            BallBounce.hitting = false;
+        }
+    }
+
+    private void ResetBall()
+    {
+        GetComponent<Rigidbody>().velocity = Vector3.zero;
+        transform.position = initialPos;
+        bounceCount = 0;
+        returning = false;
+    }
 }
--- Assets/Scenes/handball/ballBounce.cs
+++ Assets/Scenes/handball/ballBounce.cs
@@ -1,34 +1,75 @@
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using UnityEngine;
 
-public class ballBounce : MonoBehaviour
+public class BallBounce : MonoBehaviour
 {
     public Transform aimTarget;
+    public Transform ball; 
+    public Transform court;
     float speed = 3f;
     float force = 10;
-    bool hitting;
+    public static bool hitting;
+    float distanceThreshold = 2f;
+
+
+    void Start()
+    {
+            GameObject handball = GameObject.Find("Handball");
+            GameObject handballCourt = GameObject.Find("Court");
+            court = handballCourt.transform;
+            if(handball != null)
+            {
+                ball = handball.transform;
+                Debug.Log("Handball is in scene.");
+            } else {
+                Debug.LogError("Handball is not in scene.");
+            }
+
+    }
 
     void Update(){
 
         float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
 
-        if (Input.GetKeyDown(KeyCode.Space)){
-            hitting = true;
-        }else if (Input.GetKeyUp(KeyCode.Space)){
-            hitting = false;
+        if (h != 0 || v != 0){
+            transform.Translate( new Vector3(h, 0, v) * speed * Time.deltaTime);
         }
 
-        if (hitting){
-            // aimTarget.Translate( new Vector3(h, 0, 0) * speed * Time.deltaTime);
+        float distance = Vector3.Distance(transform.position, ball.position);
+
+        if (distance <= distanceThreshold){
+            // Debug.Log("Ball is hittable.");
+
+            if (Input.GetKeyDown(KeyCode.Space)){
+                hitting = true;
+                Debug.Log("Ball is hit.");
+                BallHit();
+            }else if (Input.GetKeyUp(KeyCode.Space)){
+                hitting = false;
+                Debug.Log("Ball is missed.");
+            }
         }
+        
+
+
+        // if (hitting){
+        //     // aimTarget.Translate( new Vector3(h, 0, 0) * speed * Time.deltaTime);
+        // }
     }
 
-    private void OnTriggerEnter(Collider other){
-        if(other.CompareTag("Handball"))
-        {
+    // private void OnTriggerEnter(Collider other){
+    //     if(other.CompareTag("Handball"))
+    //     {
+    //         Vector3 dir = aimTarget.position - transform.position;
+    //         other.GetComponent<Rigidbody>().velocity = dir.normalized * force + new Vector3(0, 13, 0);
+    //     }
+    // }
+
+    private void BallHit(){
             Vector3 dir = aimTarget.position - transform.position;
-            other.GetComponent<Rigidbody>().velocity = dir.normalized * force + new Vector3(0, 13, 0);
-        }
+            ball.GetComponent<Rigidbody>().velocity = dir.normalized * force + new Vector3(0, 13, 0);
     }
 }
